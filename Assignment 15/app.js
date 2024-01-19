a7a6f79b1b7da37465328ef470bd074e8f6ec250
Ali Shakeel <148653842+AliShakeel62@users.noreply.github.com>
2024-01-20 01:48:43 +0500

    Add files via upload

---
+++ Assignment 15/app.js
@@ -0,0 +1,100 @@
+// chp 31 
+
+// var dObj = new Date();
+
+
+// var dStr = new Date().toString();
+// console.log(dStr)
+
+// var date = new Date();
+// date.setFullYear("2001")
+// console.log(date)
+
+// function currentdate(month){
+//     var todaytime = new Date()
+//     todaytime.setDate(month)
+//     console.log(todaytime)
+// }
+// currentdate ("3")
+
+
+// function currentTime (time){
+//     var hours = new Date();
+//     hours.setHours(time)
+//     return hours
+// }
+// var a = currentTime("10");
+// console.log(a);
+
+
+// function q13 (age){
+//     var bornyear = new Date(age).getFullYear();
+//     var year = new Date().getFullYear();
+//     var total = year-bornyear ;
+//     return total
+// }
+// var a = q13("2008");
+// console.log(a)
+
+
+// chp 35-37
+
+
+// function first (){
+//     alert("Hey")
+// }
+// first()
+
+
+// function a (){
+//    var b = prompt("what is your name")
+//    return b
+// }
+
+// var c = a();
+// console.log(c)
+
+
+// function q12 (){
+//   var a = "letter";
+
+//   for (var i=0;i<a.length;i++){
+//     var b = a[i].indexOf(a[i])
+//  console.log(b)
+//   }
+// }
+// q12()
+
+
+// function a (word){
+// var b = word.split("").reverse().join("").toUpperCase()
+// word=word.toUpperCase()
+
+// console.log(b)
+// if (b == word){
+//   console.log("palindrome")
+// }
+// else{
+//   console.log("not")
+// }
+
+// }
+// a("Wow")
+
+
+// function q13 (word){
+// var a = word.split(" ")
+// console.log(a) 
+// for (var i = 0 ; i < a.length ; i++ ){
+//   a[i]=a[i].slice(0,1).toUpperCase() +  a[i].slice(1).toLowerCase()
+// }
+// var result =a.join("")
+// return result
+// }
+// var word = "the boy"
+// var l = q13(word)
+// console.log("results:",l)
+
+
+
+
